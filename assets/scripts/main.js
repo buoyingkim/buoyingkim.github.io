7b7e9d48abfb531194e720a8be1906d5f0f9ec79
buoyingkim <youngkim.designer@gmail.com>
2023-10-23 15:46:15 +0800

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,7 +11,14 @@ var keywordW = keyword[0].offsetWidth;
 keyword.forEach(function(keyword) {
     var top = Math.random() * ( height - keywordH );
     var left = Math.random() * ( width - keywordW );
-
+    var hue = getRandomInt(0, 360)
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
     keyword.style.marginTop = top + 'px';
     keyword.style.marginLeft = left + 'px';
+    keyword.style.color = 'hsl(H, 100%, 40%)'.replace(/H/g, hue);
 })
