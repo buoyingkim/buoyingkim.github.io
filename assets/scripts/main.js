c6e06b884f1010984b83931579a5423d185fa73a
buoyingkim <youngkim.designer@gmail.com>
2023-10-23 11:50:40 +0800

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,20 +2,22 @@ var defaultMax = Math.exp(6) / 12;  // e⁶ / 12 = 33.5em
 
 // random buffer to give for spacing.
 // growth is inverse exponential, so larger is less likely
-function buffer(min=0.1, max=defaultMax, mult=1) {
+function buffer(min=1, max=defaultMax, mult=1) {
   return Math.min(max, Math.max(min,
     min / 2 + Math.exp(Math.random() * 6) * Math.random() * mult / 12
   ))+"em";
 }
 function randomize() {
-  var posts = document.getElementsByClassName("posts");
-  for (var p = 0; p < posts.length; p++) {
+  var keyword = document.getElementsByClassName("keyword");
+  for (var p = 0; p < keyword.length; p++) {
     // random buffered margins, ordered: top right bottom left.
     // top is at least 0.1em, right and bottom are at least 0.25em.
     // top and bottom are cut in half to limit lost vertical space.
-    posts[p].style.margin = buffer(0.1,  defaultMax, 0.5) + " "
-                          + buffer(0.25) + " "
-                          + buffer(0.25, defaultMax, 0.5) + " "
-                          + buffer();
+    keyword[p].style.padding = buffer(1) + " "
+                          + buffer(1) + " "
+                          + buffer(1, defaultMax, 0.5) + " "
+                          + buffer(1);
+    keyword[p].style.width = buffer(1);
+    keyword[p].style.height = buffer(0);
   }
 }
